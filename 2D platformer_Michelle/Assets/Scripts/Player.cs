eae838845d71915c6dbb31120f0f87742b11b563
alastaip <38682393+alastaip@users.noreply.github.com>
2018-05-20 20:08:03 +1000

    180520 Some fixes
    - fixed character abnormal jumping (BUG#1)
    - implemented BUG LIST (in GDrive)

--- 2D platformer_Michelle/Assets/Scripts/Player.cs
+++ 2D platformer_Michelle/Assets/Scripts/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
     private float accelerationTimeAirborne = .2f;
     private float accelerationTimeGrounded = .1f;
     public float moveSpeed = 6f;
-	private float smoothTimeX;
+    private float smoothTimeX;
 
     public Vector2 wallJumpClimb;
     public Vector2 wallJumpOff;
@@ -32,15 +32,15 @@ public class Player : MonoBehaviour
     private Vector2 directionalInput;
     private bool wallSliding;
     private int wallDirX;
-	private float disableInputTime = 0.0f;
+    private float disableInputTime = 0.0f;
 
-	[Header ("Dash")]
-	public bool canDash = true;
-	private float nextDash;
-	private float endDash;
-	public float dashCooldown = 2;
-	public float dashSpeed = 10f;
-	public float dashTime = 0.75f;
+    [Header("Dash")]
+    public bool canDash = true;
+    private float nextDash;
+    private float endDash;
+    public float dashCooldown = 2;
+    public float dashSpeed = 10f;
+    public float dashTime = 0.75f;
 
     [Header("AnimationBools")]
     public bool animTravelLeft;
@@ -48,7 +48,7 @@ public class Player : MonoBehaviour
     public bool animGrounded;
     public bool animDoubleJump;
     public bool animWallClimb;
-	public bool animDash;
+    public bool animDash;
 
     private Animator anim;
 
@@ -63,8 +63,8 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-		DisableInput();
-		CalculateVelocity();
+        DisableInput();
+        CalculateVelocity();
         HandleWallSliding();
 
         CalculateAnimBools();
@@ -79,34 +79,37 @@ public class Player : MonoBehaviour
 
     }
 
-	private void DisableInput()
-	{
-		if (disableInputTime > 0) {
-			PlayerInput.disableInput = true;
-			disableInputTime -= Time.deltaTime;
-		} else {
-			PlayerInput.disableInput = false;
-		}
-	}
+    private void DisableInput()
+    {
+        if (disableInputTime > 0)
+        {
+            PlayerInput.disableInput = true;
+            disableInputTime -= Time.deltaTime;
+        }
+        else
+        {
+            PlayerInput.disableInput = false;
+        }
+    }
 
     private void CalculateVelocity()
     {
 
-		// currently the directionInput gives both a direction (implictly) and degree (for joystick controller)
-		// however when the char changes direction, the char 'jumps' by ?MovementSpeed
+        // currently the directionInput gives both a direction (implictly) and degree (for joystick controller)
+        // however when the char changes direction, the char 'jumps' by ?MovementSpeed
 
-		float targetVelocityX = directionalInput.x * moveSpeed;
+        float targetVelocityX = directionalInput.x * moveSpeed;
 
-		//velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below ? accelerationTimeGrounded : accelerationTimeAirborne));
-		velocity.x = targetVelocityX;
-		velocity.y += gravity * Time.deltaTime;
+        //velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below ? accelerationTimeGrounded : accelerationTimeAirborne));
+        velocity.x = targetVelocityX;
+        velocity.y += gravity * Time.deltaTime;
 
         anim.SetFloat("velocityX", Mathf.Abs(velocity.x));
         anim.SetFloat("velocityY", Mathf.Abs(velocity.y));
 
     }
 
-	// *** ANIMATIONS *** //
+    // *** ANIMATIONS *** //
 
     private void CalculateAnimBools()
     {
@@ -129,108 +132,108 @@ public class Player : MonoBehaviour
     public void SetAnimator()
     {
         if (animTravelLeft)
-        	{transform.localScale = new Vector3(-1f, 1f, 1f);}
+        { transform.localScale = new Vector3(-1f, 1f, 1f); }
         else
-        	{transform.localScale = new Vector3(1f, 1f, 1f);}
+        { transform.localScale = new Vector3(1f, 1f, 1f); }
 
         if (animTravelDown)
-        	{anim.SetBool("falling", true);}
-        else 
-			{anim.SetBool("falling", false);}
+        { anim.SetBool("falling", true); }
+        else
+        { anim.SetBool("falling", false); }
 
         if (animGrounded)
-        	{anim.SetBool("grounded", true);}
-        else 
-			{anim.SetBool("grounded", false);}
+        { anim.SetBool("grounded", true); }
+        else
+        { anim.SetBool("grounded", false); }
 
         if (animWallClimb)
-			{anim.SetBool("wallClimb", true);}
+        { anim.SetBool("wallClimb", true); }
         else
-        	{anim.SetBool("wallClimb", false);}
+        { anim.SetBool("wallClimb", false); }
     }
 
-	// *** SPECIFIC MOTION SUBROUTINES *** //
-	public void SetDirectionalInput(Vector2 input)
-	{
-		directionalInput = input;
-	}
+    // *** SPECIFIC MOTION SUBROUTINES *** //
+    public void SetDirectionalInput(Vector2 input)
+    {
+        directionalInput = input;
+    }
 
-	public void OnJumpInputDown()
-	{
-		if (wallSliding)
-		{
-			if (wallDirX == directionalInput.x)
-			{
-				velocity.x = -wallDirX * wallJumpClimb.x;
-				velocity.y = wallJumpClimb.y;
-			}
-			else if (directionalInput.x == 0)
-			{
-				velocity.x = -wallDirX * wallJumpOff.x;
-				velocity.y = wallJumpOff.y;
-			}
-			else
-			{
-				velocity.x = -wallDirX * wallLeap.x;
-				velocity.y = wallLeap.y;
-			}
-			isDoubleJumping = false;
-		}
-		if (controller.collisions.below)
-		{
-			velocity.y = maxJumpVelocity;
-			isDoubleJumping = false;
-		}
-		if (canDoubleJump && !controller.collisions.below && !isDoubleJumping && !wallSliding)
-		{
-			velocity.y = maxJumpVelocity;
-			isDoubleJumping = true;
-		}
-	}
+    public void OnJumpInputDown()
+    {
+        if (wallSliding)
+        {
+            if (wallDirX == directionalInput.x)
+            {
+                velocity.x = -wallDirX * wallJumpClimb.x;
+                velocity.y = wallJumpClimb.y;
+            }
+            else if (directionalInput.x == 0)
+            {
+                velocity.x = -wallDirX * wallJumpOff.x;
+                velocity.y = wallJumpOff.y;
+            }
+            else
+            {
+                velocity.x = -wallDirX * wallLeap.x;
+                velocity.y = wallLeap.y;
+            }
+            isDoubleJumping = false;
+        }
+        if (controller.collisions.below)
+        {
+            velocity.y = maxJumpVelocity;
+            isDoubleJumping = false;
+        }
+        if (canDoubleJump && !controller.collisions.below && !isDoubleJumping && !wallSliding)
+        {
+            velocity.y = maxJumpVelocity;
+            isDoubleJumping = true;
+        }
+    }
 
-	public void OnJumpInputUp()
-	{
-		if (velocity.y > minJumpVelocity)
-		{
-			velocity.y = minJumpVelocity;
-		}
-	}
+    public void OnJumpInputUp()
+    {
+        if (velocity.y > minJumpVelocity)
+        {
+            velocity.y = minJumpVelocity;
+        }
+    }
 
-	public void OnDash()
-	{
-		
-	}
+    public void OnDash()
+    {
 
-	private void HandleWallSliding()
-	{
-		wallDirX = (controller.collisions.left) ? -1 : 1;
-		wallSliding = false;
-		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
-		{
-			wallSliding = true;
+    }
 
-			if (velocity.y < -wallSlideSpeedMax)
-			{
-				velocity.y = -wallSlideSpeedMax;
-			}
+    private void HandleWallSliding()
+    {
+        wallDirX = (controller.collisions.left) ? -1 : 1;
+        wallSliding = false;
+        if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
+        {
+            wallSliding = true;
 
-			if (timeToWallUnstick > 0f)
-			{
-				velocityXSmoothing = 0f;
-				velocity.x = 0f;
-				if (directionalInput.x != wallDirX && directionalInput.x != 0f)
-				{
-					timeToWallUnstick -= Time.deltaTime;
-				}
-				else
-				{
-					timeToWallUnstick = wallStickTime;
-				}
-			}
-			else
-			{
-				timeToWallUnstick = wallStickTime;
-			}
-		}
-	}
+            if (velocity.y < -wallSlideSpeedMax)
+            {
+                velocity.y = -wallSlideSpeedMax;
+            }
+
+            if (timeToWallUnstick > 0f)
+            {
+                velocityXSmoothing = 0f;
+                velocity.x = 0f;
+                if (directionalInput.x != wallDirX && directionalInput.x != 0f)
+                {
+                    timeToWallUnstick -= Time.deltaTime;
+                }
+                else
+                {
+                    timeToWallUnstick = wallStickTime;
+                }
+            }
+            else
+            {
+                timeToWallUnstick = wallStickTime;
+            }
+        }
+    }
 }
